b4d754d15d59dced7e875951af3764099b5a3c86
janbeast <65708414+janbeast@users.noreply.github.com>
2020-05-21 22:03:44 +0700

    init

---
+++ employee-profile/src/main/java/com/sittinon/employeeprofile/EmployeeProfileApplication.java
@@ -0,0 +1,13 @@
+package com.sittinon.employeeprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProfileApplication.class, args);
+	}
+
+}
